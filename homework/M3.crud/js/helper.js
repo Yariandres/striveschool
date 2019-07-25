8dd1ca184c4b75ced77544576c4b76482461bdb7
Yari Andres <yari.andres@gmail.com>
2019-07-25 09:08:26 +0200

    update sever fetch function

--- homework/M3.crud/js/helper.js
+++ homework/M3.crud/js/helper.js
@@ -1,6 +1,24 @@
     getEvents = async () => {
+
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
         const url = "https://cors-anywhere.herokuapp.com/https://strive-school-testing-apis.herokuapp.com/api/product/"; // site that doesn’t send Access-Control-*
-        var response = await fetch(proxyurl + url);
+
+        let username = 'user10';
+        let password = 'hmWhr9ktc4bejjTg';
+
+        let authString = `${username}:${password}`
+
+        let headers = new Headers({
+            "content-type": "application/json",
+        });
+
+        headers.set('Authorization', 'Basic ' + btoa(authString))
+
+        var response = await fetch(url, {
+            method: 'POST',
+            headers: headers
+        });
+
         return await response.json();
     }
