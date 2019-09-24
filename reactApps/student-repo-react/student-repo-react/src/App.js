ff25b46e54838a83703bb8c0d7b15c2618e068d5
Yari Andres <yari.andres@gmail.com>
2019-09-24 17:28:32 +0200

    fixing layout

--- reactApps/student-repo-react/student-repo-react/src/App.js
+++ reactApps/student-repo-react/student-repo-react/src/App.js
@@ -12,30 +12,26 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-
           {/* {console.log(this.state.projects)} */}
           {console.log(this.state.students)}
-          {this.state.projects.map(project => <div key={project.ID}>{project.Name} {project.Description} {project.RepoURL} <hr /></div>)}
-
+          {this.state.projects.map(project => <div key={project.ID}> {project.Name} <br /> {project.Description} <br /> {project.RepoURL} <hr /></div>)}
         </header>
       </div>
     );
   }
 
   componentDidMount = async () => {
-    // var studentResp = await fetch("http://localhost:3550/students");
-    // var studentJson = await studentResp.json();
-    // this.setState({
-    //   students: studentJson
-    // })
+    var studentResp = await fetch("http://localhost:3550/students");
+    var studentJson = await studentResp.json();
+    this.setState({
+      students: studentJson
+    })
 
     var projectResp = await fetch("http://localhost:3550/projects");
     var projectJson = await projectResp.json();
     this.setState({
       projects: projectJson
     })
-
-
   }
 }
 
