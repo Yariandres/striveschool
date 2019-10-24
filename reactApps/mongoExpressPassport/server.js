493fa71f96012dd5c32c5f31b9781b53ec39a4dd
Yari Andres <yari.andres@gmail.com>
2019-10-24 08:21:55 +0200

    adding connection to mongodb

--- reactApps/mongoExpressPassport/server.js
+++ reactApps/mongoExpressPassport/server.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const connection = mongoose.connect(config.mongoUrl, { useNewUrlParse: true })
+connection.then(db => {
+  console.log("database connected")
+},
+  err => console.log(err)
+);
 
 var server = express();
 
