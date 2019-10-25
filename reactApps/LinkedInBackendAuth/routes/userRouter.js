86305d1be4fc5195f56412183872973951fce58b
Yari Andres <yari.andres@gmail.com>
2019-10-25 17:02:27 +0200

    server with DB final

--- reactApps/LinkedInBackendAuth/routes/userRouter.js
+++ reactApps/LinkedInBackendAuth/routes/userRouter.js
@@ -1,18 +1,66 @@
-const express = require("express");
-const User = require("../models/user");
-const passport = require("passport");
+const express = require("express")
+const User = require("../models/user")
+const passport = require("passport")
+const { createToken } = require("../auth")
 
-const router = express.Router();
+const router = express.Router()
+
+router.get("/", async (req, res) =>{
+    res.send(await User.find({}))
+})
+
 
 router.post("/signup", async (req, res) => {
-  try {
-    var user = await User.register(req.body, req.body.password)
-    res.send(user)
-  }
-  catch (exx) {
-    res.statusCode = 500;
-    res.send(exx)
-  }
+    try{
+        var user = await User.register(req.body, req.body.password)
+        res.send(user)
+    }
+    catch(exx){
+        res.statusCode = 500;
+        res.send(exx)
+    }
+})
+
+router.post("/login", passport.authenticate("local"), (req, res)=>{
+    var token = createToken({ _id : req.user._id, username: req.user.username, email: req.user.email})
+    res.send({
+        success: true,
+        username: req.user.username,
+        email: req.user.email,
+        token: token
+    })
+})
+
+//1) verify the current token
+router.post("/refresh", passport.authenticate("jwt"), (req, res)=>{
+    //2) create the new token
+    var token = createToken({ _id: req.user._id, username: req.user.username});
+    //3) send back the new token
+    res.send({
+        success:true,
+        username: req.user.username,
+        token: token
+    })
+})
+
+router.post("/verifyCredentials", passport.authenticate("local"), (req, res)=>{
+    var token = createToken({ _id : req.user._id, username: req.user.username, email: req.user.email})
+    res.send({
+        username: req.user.username,
+        email: req.user.email,
+        token: token
+    })
+})
+
+// NO DATABASE INVOLVES = NO SESSION
+// router.post("/verifyToken", passport.authenticate("jwt", { session: false }), (req,res)=>{
+//     res.send(req.user)
+// })
+
+router.post("/verifyToken", passport.authenticate("jwt"), (req,res)=>{
+    res.send(req.user)
 })
 
 module.exports = router;
+
+
--- reactApps/LinkedInBackendAuth/server.js
+++ reactApps/LinkedInBackendAuth/server.js
@@ -1,33 +1,31 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const userRouter = require("./routes/userRouter");
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const userRouter = require("./routes/userRouter")
+const auth = require("./auth")
 const passport = require("passport")
 
+//this could be your localhost mongodb address
 const url = "mongodb+srv://yari_striveschool:tMJ6V66Wd8wyKsvA@linkedinback-gfzzk.mongodb.net/test?retryWrites=true&w=majority"
-const connection = mongoose.connect(url, { useUnifiedTopology: true });
-
-// check if ther is any errors connecting to the DB
+const connection = mongoose.connect(url, { useNewUrlParser: true })
 connection.then(db => {
-  console.log("Database up and Running")
+    console.log("Database up & running")
 },
-  err => {
-    console.log(err);
-  }
-);
+    err => {
+        console.log(err)
+    })
 
-var server = express();
-server.use(cors());
-server.use(express.json());
-server.use(passport.initialize());
+var server = express()
+server.use(cors())
+server.use(express.json()) //equal to body-parser
+server.use(passport.initialize())
 
-server.use("/users", userRouter);
+server.use("/users", userRouter)
 
 server.get("/", (req, res) => {
-  res.send("You are connected to the DB and server is running!");
-});
+    res.send("Hello World")
+})
 
-var port = 3333;
-server.listen(port, () => {
-  console.log(`Server is running on ${port}`)
-});
+server.listen(3333, () => {
+    console.log("Server is listening on port 3333")
+})
