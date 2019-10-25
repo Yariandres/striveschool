a84e84bcbdc47db06541ff9d757134192d1e8e2a
Yari Andres <yari.andres@gmail.com>
2019-10-25 12:26:18 +0200

    latet

--- reactApps/LinkedInBackendAuth/routes/userRouter.js
+++ reactApps/LinkedInBackendAuth/routes/userRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 
-const router = express.router();
+const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   // cretae a new user => user.register
--- reactApps/LinkedInBackendAuth/server.js
+++ reactApps/LinkedInBackendAuth/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRouter = require("./routes/userRouter");
 
 const url = "mongodb+srv://yari_striveschool:tMJ6V66Wd8wyKsvA@linkedinback-gfzzk.mongodb.net/test?retryWrites=true&w=majority"
 const connection = mongoose.connect(url, { useUnifiedTopology: true });
@@ -17,6 +18,8 @@ connection.then(db => {
 var server = express();
 server.use(cors());
 server.use(express.json());
+
+server.use("/users", userRouter);
 
 server.get("/", (req, res) => {
   res.send("You are connected to the DB and server is running!");
