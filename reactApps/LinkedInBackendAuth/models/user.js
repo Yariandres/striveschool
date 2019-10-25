f6fd64a5f7bd10c54d3ac935c2ce263f39f9eebd
Yari Andres <yari.andres@gmail.com>
2019-10-25 12:03:57 +0200

    adding user register

---
+++ reactApps/LinkedInBackendAuth/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+var User = new mongoose.Schema({
+
+});
+
+User.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", User);
---
+++ reactApps/LinkedInBackendAuth/routes/userRouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const User = require("../models/user");
+
+const router = express.router();
+
+router.post("/signup", async (req, res) => {
+  // cretae a new user => user.register
+  try {
+    var user = await User.register(req.body, req.body.password);
+    res.send(user);
+  }
+  catch (exx) {
+    res.statusCode = 500;
+    res.send(exx);
+  }
+});
+
+module.exports = router;
