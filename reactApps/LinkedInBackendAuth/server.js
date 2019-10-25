2198ca61c2dc8cd178f18a944a183112bf64f982
Yari Andres <yari.andres@gmail.com>
2019-10-25 11:38:18 +0200

    adding server.js

---
+++ reactApps/LinkedInBackendAuth/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const url = "mongodb+srv://yari_striveschool:tMJ6V66Wd8wyKsvA@linkedinback-gfzzk.mongodb.net/test?retryWrites=true&w=majority"
+const connection = mongoose.connect(url, { useUnifiedTopology: true });
+
+// check if ther is any errors connecting to the DB
+connection.then(db => {
+  console.log("Database up and Running")
+},
+  err => {
+    console.log(err);
+  }
+);
+
+var server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("You are connected to the DB and server is running!");
+});
+
+var port = 3333;
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+});
